3bf9e5bad27a6ec8c39fb75a000f168d811b371c
aravadani <aravadani@users.noreply.github.com>
2020-09-08 17:26:29 -0700

    Star Update ✨

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,40 @@
-/* let IMG = document.querySelector("img");
-var check = true;
-function flip() {
-  if(check)
-  {
-    IMG.src = "images/Wink.png";
-    check = !check;
-  }
-  else{
-    IMG.src = "images/Smile.png";
-    check = !check;
-  }
+
+function pathOnDiv(no, pos)
+{
+   // Get the coordinates of the point that is the fraction 'pos' along the path
+   var  path = document.getElementById("mypath");
+   var  pathLength = path.getTotalLength();
+   var  loc = path.getPointAtLength(pos * pathLength);
+   let obj = {
+     x: loc.x,
+     y: loc.y,
+     no: no
+   };
+   iconPos.push(obj);
+   //console.log(loc);
+   var el = document.querySelector(icons[no]);
+   el.style.left = loc.x + "px";
+   el.style.top = loc.y + "px";
 }
- window.setInterval(flip, 700);
+
+let icons = [".mail", ".linkedin", ".instagram", ".github", ".troylabs", ".spotify"];
+let iconPos = [];
+
+pathOnDiv(2, 0/6);
+pathOnDiv(3, 1/6);
+pathOnDiv(4, 2/6);
+pathOnDiv(5, 3/6);
+pathOnDiv(0, 4/6);
+pathOnDiv(1, 5/6);
+
+console.log(iconPos);
+
+/*
+anime({
+  targets: '.mail',
+  top: iconPos[].x + "px",
+  left: iconPos[].y + "px",
+  delay: 1000,
+  loop: true
+});
 */
-//onmouseover="this.src='images/Wink.png';" onmouseout="this.src='images/Smile.png';"
